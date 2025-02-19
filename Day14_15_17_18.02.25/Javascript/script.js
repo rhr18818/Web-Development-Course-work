8bb2fb4df0bbd3cd22565be5824819be2da812ef
rhr18818 <rhr14101@gmail.com>
2025-02-19 19:19:35 +0600

    update

---
+++ Day14_15_17_18.02.25/Javascript/script.js
@@ -0,0 +1,78 @@
+// // Assignment 1
+// var age = Number(prompt("What is your age?"));
+// if(age<18){
+//     console.log("You are Minor");
+    
+// }else if(age>60){
+//     console.log("You are a Senior Citizen");
+    
+// }else{
+//     console.log("You are an Adult");
+    
+// }
+
+
+// // Assignment 2
+// var num =Number(prompt("Enter Number: "));
+// if(num%2==0){
+//     console.log("Even NUmber");
+    
+// }else{
+//     console.log("Odd Number");
+    
+// }
+
+// Assignment 3
+
+// var char = prompt("Enter your character:");
+// // if(char>'a' && char<'z'){
+// //     console.log("small letter");
+    
+// // }else if(char>'A' && char <'Z'){
+// //     console.log("Capital Letter");
+    
+// // }else{
+// //     console.log("Not a letter");
+    
+// // }
+// if(char===char.toLowerCase()){
+//     console.log("small letter");
+    
+// }else if(char===char.toUpperCase()){
+//     console.log("Capital Letter");
+    
+// }else{
+//     console.log("Not a letter");
+    
+// }
+
+
+// // Assignment 4
+
+// var num1= Number(prompt("Number1: "))
+// var num2= Number(prompt("Number2: "))
+// var num3= Number(prompt("Number3: "))
+
+// console.log(Math.max(num1,num2,num3));
+
+
+// 5-6-7 basic problem
+
+// // 8
+// var name = prompt("Your Name: ");
+// const d = new Date();
+
+
+///13
+
+// var num1 = Number(prompt("num1:"))
+// var num2 = Number(prompt("num2:"))
+
+// num1= num1+num2
+// num2 = num1-num2
+// num1 = num1-num2
+
+// console.log("Number1: "+ num1 +" \n "+"Number2: "+ num2);
+
+var num= '123'
+console.log(num.reverse());
