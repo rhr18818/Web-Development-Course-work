56d3e2eaab0d64e204a5f4e01afd9922c9830def
rhr18818 <rhr14101@gmail.com>
2025-03-03 14:22:00 +0600

    Day 15 due assignment

--- Day14_15_17_18.02.25/Javascript/script.js
+++ Day14_15_17_18.02.25/Javascript/script.js
@@ -59,9 +59,34 @@
 // 5-6-7 basic problem
 
 // // 8
-// var name = prompt("Your Name: ");
-// const d = new Date();
+//  var name = prompt("Your Name: ");
+// //  var time = Number(prompt("Enter Time: "));
+//  const d = new Date();
+//  var time = d.getHours();
+// if(time>=5 && time<12){
+//     console.log("Good Morning,"+ name);
+// }
+// else if(time>=12 && time<17){
+//     console.log("Good Afernoon,"+ name);
+// }
+// else if(time>=17 && time<21){
+//     console.log("Good Evening"+ name);
+// }
+// else{
+// //     console.log("Good Night"+ name);
+// }
 
+// //Assignment 10
+// var num1 = Number(prompt("num1:"))
+// var arr =[]
+// for(var i=1; i<=10;i++){
+//     arr.push(num1+"*"+i+" = "+ (num1*i))
+//     // console.table(num1+"*"+i+" = "+ (num1*i))
+//     // console.log(num1+"*"+i+"= "+ num1*i)
+
+
+// }
+// console.table(arr)
 
 ///13
 
@@ -74,5 +99,48 @@
 
 // console.log("Number1: "+ num1 +" \n "+"Number2: "+ num2);
 
-var num= '123'
-console.log(num.reverse());
+// var num= '123'
+// console.log(num.reverse());
+
+
+// //Assignment 16
+
+// var num = prompt("Enter Your Number :")
+// var sum=0
+// // for(var char of num){
+// //         sum += Number(char);
+// // }
+// // console.log(sum);
+
+// num.split('').forEach((char,index )=> {
+//     console.log(`${index}: ${char}`);
+// });
+
+// //Assignment 17
+
+// var num = prompt("Enter Your String :")
+// var len = num.length;
+// var check;
+// for(var i=0;i<len/2;i++){
+//         if(num[i]!==num[len-i-1]){
+//             // console.log(num[i] + num[len-i-1]);
+            
+//             check = true;
+//             break;
+//         } 
+// }
+// if(check){
+//     console.log("It's not Palindrom");
+// }
+// else{
+//     console.log("It's Palindrom");
+// }
+// //Assignment 18
+
+var num = prompt("Enter Your String :")
+var arr=[]
+for(var i=num.length-1;i>=0;i--){
+        arr.push(num[i]);
+        console.log(num[i]);    
+}
+console.log(arr.toString().replaceAll(',',''));
