abbd29331e9d32fa5f5091b06abe8b01a07f992d
rhr18818 <rhr14101@gmail.com>
2025-03-21 22:39:33 +0600

    more new things - left and right rotation by 1 - with swap ------other approaches

--- DSA from Day_26/Day36_20.03/leftshift.js
+++ DSA from Day_26/Day36_20.03/leftshift.js
@@ -1,5 +1,19 @@
+// Approach 1
+
 let arr=[1,2,3,4,5]
 for(let i=0;i<arr.length-1;i++){
     [arr[i],arr[i+1]]=[arr[i+1],arr[i]];
 }
+
+// Approach 2
+
+// let temp = arr[0]
+// for(let i=0;i<arr.length-1;i++){
+//     arr[i]=arr[i+1]
+// }
+// arr[arr.length-1]=temp;
+
+
 console.log(arr);
+
+
