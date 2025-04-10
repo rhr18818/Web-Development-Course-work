4af83ffd429ecc9024c60c3a8d5e6cc4477776ff
rhr18818 <rhr14101@gmail.com>
2025-04-10 22:45:00 +0600

    update day 52

--- DSA from Day_26/Day52_10.04/count_pref.js
+++ DSA from Day_26/Day52_10.04/count_pref.js
@@ -1,3 +1,13 @@
 // 1. By using of startsWith -> function 
 
-// - study first : 
+// - study first : take two parameter , and return only true and false
+//one's for - prefix 
+//another - for which position, default is = 0 
+
+let s = ["pay","attention","practise","attend"]
+let pref = "at"
+let count =0;
+for(let i in s){
+    if(s[i].startsWith(pref)) count ++
+}
+console.log(count);
