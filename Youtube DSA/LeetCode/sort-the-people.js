9ed3af7b24de8e78267f40a2ef86a057bd65b90b
rhr18818 <rhr14101@gmail.com>
2025-04-15 22:42:54 +0600

    Previous Youtube DSA--SortPeople solved

--- Youtube DSA/LeetCode/sort-the-people.js
+++ Youtube DSA/LeetCode/sort-the-people.js
@@ -1,8 +1,18 @@
+// very wierd approach ---don't where did i come out with this ! -
+// - couldn't get how the set working----have to use map here !
+
 var heights = [17233,32521,14087,42738,46669,65662,43204,8224]
 var names = ["IEO","Sgizfdfrims","QTASHKQ","Vk","RPJOFYZUBFSIYp","EPCFFt","VOYGWWNCf","WSpmqvb"]
 
 var sortPeople = function(names, heights) {
-    var set = new Set()
+    var set = new Set()//// var set ={}
+
+    // Set is being used like an object. This works only because 
+    // JavaScript allows objects to have custom properties, 
+    // even if they're instances of Set.
+    // This is bad practice and confusing.
+    // Use a Map or a plain {} object if you want key-value pairs.
+
     var s = []
     for(let i=0;i<names.length;i++){
         set[heights[i]] = names[i]
