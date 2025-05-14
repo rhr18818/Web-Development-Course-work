6456f0706c83dcadcb9efbb64d198a1cdd33e8d6
rhr18818 <rhr14101@gmail.com>
2025-05-14 15:16:15 +0600

    leetcode 268

--- Youtube DSA/LeetCode/268_missing_number.js
+++ Youtube DSA/LeetCode/268_missing_number.js
@@ -8,6 +8,14 @@ var missingNumber = function(nums) {
     // }
      
     //2nd ---> 0 to n --> natural num sum -> formula
+    // let res = nums.length;
+        
+    // for (let i = 0; i < nums.length; i++) {
+    //         res += i - nums[i];
+    //     }
+        
+    // return res;
+
 
     let n = nums.length
     let result = (n*(n+1))/2
@@ -19,7 +27,7 @@ var missingNumber = function(nums) {
 
     // 3rd solution by xor
 
-    // let xor = 0
+    // let xor = 0 // if give error --> xor = nums.length and loop i< nums.length !
     // let nums = [9,6,4,2,3,5,7,0,1]
     // for(let i=0;i<=nums.length;i++){
     //     xor ^= i^nums[i]
