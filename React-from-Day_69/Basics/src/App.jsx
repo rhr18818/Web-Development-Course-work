5186ab1b9836feffc39682f31d89c7fc93ba63ef
rhr18818 <rhr14101@gmail.com>
2025-06-01 02:33:03 +0600

    Day 72 ---> Basic Data Type

--- React-from-Day_69/Basics/src/App.jsx
+++ React-from-Day_69/Basics/src/App.jsx
@@ -1,30 +1,29 @@
-
 const App = ()=>{
-  function handleClick(){
-    alert("Button Clicked")
+  let n = 12 //number
+  let s = "Ratul" //string
+  let b = false //boolean ---> will not show -- reserved for JS
+  let nu = null //  ---> will not show -- reserved for JS
+  let un = undefined //  ---> will not show -- reserved for JS
+  let arr = [12,'Ratul',false,null]
+  let arr2 = [<h1>Hey From Array ...</h1>,12,'Ratul']
+  let obj = {
+    name : 'Ratul',
+    age : 35
   }
-  const handleParamClick = (msg)=>{
-    alert(msg)
-  }
-  // function wrapperFuntion(){
-  //   handleParamClick("Hello from Param Function")
-  // }
-
-  /// optimized one !!
-
-  //const wrapperFuntion = ()=> handleParamClick("Hello from Param Function")
-
   return(
     <>
       <h1 style={{color: 'tomato', textAlign: 'center'}}>Hello {2+3}</h1>
-      <h2 style={{color: 'tomato', textAlign: 'center'}}>Hi</h2>
-      <button onClick={handleClick}>Click</button>
-      {/* <button onClick={wrapperFuntion}>Click-2</button> */}
-      <button onClick={()=> handleParamClick("Hello from Param Function")}>Click-2</button>
+      <h2 style={{textAlign: 'center'}}>Number: {n}</h2>
+      <h2 style={{textAlign: 'center'}}>String: {s}</h2>
+      <h2 style={{textAlign: 'center'}}>Boolean: {b}</h2>
+      <h2 style={{textAlign: 'center'}}>Null: {nu}</h2>
+      <h2 style={{textAlign: 'center'}}>Undefined: {un}</h2>
+      <h2 style={{textAlign: 'center'}}>Array: {arr}</h2>
+      <h2 style={{textAlign: 'center'}}>Array with JSX: {arr2}</h2>
+      <h2 style={{textAlign: 'center'}}>Object: {obj.name} || {obj.age}</h2>
+
+      
     </>
   )
 }
-//console.log("checking");
-// export const a = "Hello"
-// export const b = "World"
 export default App
