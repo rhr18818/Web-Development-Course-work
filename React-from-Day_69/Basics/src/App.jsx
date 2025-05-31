f05e5567fb478be50325f8a6772d1183fbda89c6
rhr18818 <rhr14101@gmail.com>
2025-06-01 01:55:20 +0600

    Day 71---> Ract hood class Practise

--- React-from-Day_69/Basics/src/App.jsx
+++ React-from-Day_69/Basics/src/App.jsx
@@ -1,13 +1,30 @@
-import React from "react";
 
 const App = ()=>{
+  function handleClick(){
+    alert("Button Clicked")
+  }
+  const handleParamClick = (msg)=>{
+    alert(msg)
+  }
+  // function wrapperFuntion(){
+  //   handleParamClick("Hello from Param Function")
+  // }
+
+  /// optimized one !!
+
+  //const wrapperFuntion = ()=> handleParamClick("Hello from Param Function")
+
   return(
-    <div><h1 style={{color: 'tomato', textAlign: 'center'}}>Hello</h1></div>
+    <>
+      <h1 style={{color: 'tomato', textAlign: 'center'}}>Hello {2+3}</h1>
+      <h2 style={{color: 'tomato', textAlign: 'center'}}>Hi</h2>
+      <button onClick={handleClick}>Click</button>
+      {/* <button onClick={wrapperFuntion}>Click-2</button> */}
+      <button onClick={()=> handleParamClick("Hello from Param Function")}>Click-2</button>
+    </>
   )
 }
 //console.log("checking");
-
-
-export const a = "Hello"
-export const b = "World"
+// export const a = "Hello"
+// export const b = "World"
 export default App
--- React-from-Day_69/Basics/src/main.jsx
+++ React-from-Day_69/Basics/src/main.jsx
@@ -1,9 +1,5 @@
 import { createRoot } from "react-dom/client"
 import App from './App.jsx'
 
-import { a,b } from './App.jsx'
-import { StrictMode } from "react"
-console.log(a,b)
-
-createRoot(document.getElementById("root")).render(<App/>)
+createRoot(document.getElementById("root")).render(<App></App>)
 
