e953348c9cc564c99dfab04c1edde34315807027
rhr18818 <rhr14101@gmail.com>
2025-06-02 16:26:09 +0600

    Day 76--Basic form option--radio,select,checkbox

--- React-from-Day_69/Basics/src/Day76.jsx
+++ React-from-Day_69/Basics/src/Day76.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Day76 = () => {
+    const [todos, settodos] = useState([
+        {id:1,title:"Something.......",isCompleted: false}
+    ])
+
+    const [Title, setTitle] = useState('')
+    const [Completed, setCompleted] = useState(true)
+    const [gender, setgender] = useState('male')
+    const [city, setcity] = useState("dhaka")
   return (
-    <div>Day76</div>
+    <>
+        <h1>To-Do</h1>
+        <h2>Create Task</h2>
+        <form >
+            <input type="text" 
+            placeholder='title'
+            onChange={(e)=>setTitle(e.target.value)}
+            value={Title}
+            /><br />
+            <input 
+            type="checkbox" 
+            onChange={(e)=> setCompleted(e.target.checked)}
+            checked={Completed}
+            /> Completed
+            <br />
+
+            <input 
+            type="radio" 
+            value="male"
+            checked={gender=="male" && true}
+            onChange={(e)=> setgender(e.target.value)}
+            //onChange={(e)=> console.log(e)}
+            /> Male
+            <input 
+            type="radio" 
+            value="female"
+            checked={gender=="female" && true}
+            onChange={(e)=> setgender(e.target.value)
+            }
+            /> Female
+            <br />
+            <br />
+            <select value={city} onChange={(e)=> setcity(e.target.value)}>
+                <option value="khulna">Khulna</option>
+                <option value="dhaka">Dhaka</option>
+                <option value="sylhet">Sylhet</option>
+            </select>
+            <br />
+
+            <button>Create todo</button>
+        </form>
+    </>
   )
 }
 
--- React-from-Day_69/Basics/src/main.jsx
+++ React-from-Day_69/Basics/src/main.jsx
@@ -3,6 +3,7 @@ import App from './App.jsx'
 import Json from "./Day72.jsx"
 import { A } from "./Day72.jsx"
 import Day75 from "./Day75.jsx"
+import Day76 from "./Day76.jsx"
 
-createRoot(document.getElementById("root")).render(<Day75></Day75>)
+createRoot(document.getElementById("root")).render(<Day76></Day76>)
 
