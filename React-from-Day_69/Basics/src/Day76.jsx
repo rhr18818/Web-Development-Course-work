0fd959a8446ff9f39d5344270c5d0ec598971073
rhr18818 <rhr14101@gmail.com>
2025-06-02 17:01:52 +0600

    Day 76--todo-project v1

--- React-from-Day_69/Basics/src/Day76.jsx
+++ React-from-Day_69/Basics/src/Day76.jsx
@@ -1,58 +1,58 @@
 import React from 'react'
 import { useState } from 'react'
+import { nanoid } from 'nanoid'
+
 const Day76 = () => {
     const [todos, settodos] = useState([
         {id:1,title:"Something.......",isCompleted: false}
     ])
 
     const [Title, setTitle] = useState('')
-    const [Completed, setCompleted] = useState(true)
-    const [gender, setgender] = useState('male')
-    const [city, setcity] = useState("dhaka")
+    function submitHandler(e){
+        e.preventDefault()
+        const newTodo = {
+            id : nanoid(),
+            title:Title,
+            isCompleted: false
+        }
+        
+        let copytodos = [...todos]
+        copytodos.push(newTodo)
+        settodos(copytodos)
+
+        //settodos([...todos,newTodo])
+        
+    }
+    console.log(todos);
+
+    const renderTodo = todos.map(todo =>{
+        return <li key={todo.id}>{todo.title}</li>
+    })
   return (
     <>
         <h1>To-Do</h1>
         <h2>Create Task</h2>
-        <form >
+        <form onSubmit={submitHandler}>
             <input type="text" 
             placeholder='title'
             onChange={(e)=>setTitle(e.target.value)}
             value={Title}
             /><br />
-            <input 
-            type="checkbox" 
-            onChange={(e)=> setCompleted(e.target.checked)}
-            checked={Completed}
-            /> Completed
-            <br />
-
-            <input 
-            type="radio" 
-            value="male"
-            checked={gender=="male" && true}
-            onChange={(e)=> setgender(e.target.value)}
-            //onChange={(e)=> console.log(e)}
-            /> Male
-            <input 
-            type="radio" 
-            value="female"
-            checked={gender=="female" && true}
-            onChange={(e)=> setgender(e.target.value)
-            }
-            /> Female
-            <br />
-            <br />
-            <select value={city} onChange={(e)=> setcity(e.target.value)}>
-                <option value="khulna">Khulna</option>
-                <option value="dhaka">Dhaka</option>
-                <option value="sylhet">Sylhet</option>
-            </select>
+            
             <br />
 
             <button>Create todo</button>
         </form>
+        <br />
+        <hr />
+        <h2>Created-Task's</h2>
+        <ul>
+            {renderTodo}
+        </ul>
     </>
   )
 }
 
 export default Day76
+
+
