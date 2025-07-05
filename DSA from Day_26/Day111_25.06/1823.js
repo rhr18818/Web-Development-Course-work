5e19b863436cb71911a874f4b82a65e75192c6fa
rhr18818 <rhr14101@gmail.com>
2025-07-05 20:33:34 +0600

    Day 111--DSA ---josephous--> claude visual html

--- DSA from Day_26/Day111_25.06/1823.js
+++ DSA from Day_26/Day111_25.06/1823.js
@@ -20,3 +20,14 @@ var findTheWinner = function(n, k) {
     k= k-1;
     return solver(k,arr,0);
 };
+
+
+//// watch love Babber Video for Better understand 
+
+function solver(n,k){
+    if(n==1) return 0
+    return (solver(n-1,k)+k)%n
+}
+var findTheWinner = function(n, k) {
+    return solver(n,k)+1
+};
