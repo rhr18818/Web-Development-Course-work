952734f753e461f83603017dee5bea0aa74777ae
rhr18818 <rhr14101@gmail.com>
2025-07-08 22:39:54 +0600

    Day122--> Backend--> Database conccetion and CRUD operation

---
+++ Backend from Day_115/Day122_08.07/server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const connectToDB = require('./src/db/db')
+const noteModel = require('./src/models/note.model')
+
+connectToDB()
+const server = express()// name can be anything
+
+server.use(express.json())
+server.post('/notes',async (req,res)=>{
+    const{title,content} = req.body
+    console.log(title,content);
+
+    await noteModel.create({
+        title,content
+    })
+    res.json({
+        message: "Note created successfully.!"
+    })
+})
+
+server.get('/notes', async(req,res)=>{
+    const notes = await noteModel.find()
+    res.json({
+        message: "Note fetched Successfully",
+        notes
+    })
+})
+
+server.delete('/notes/:id', async(req,res)=>{
+    const noteId = req.params.id
+    await noteModel.findOneAndDelete({
+        _id: noteId
+    })
+    res.json({
+        message: "Note Deleted Successfully",
+    })
+})
+
+server.patch('/notes/:id', async(req,res)=>{
+    const noteId = req.params.id
+    const {title} = req.body
+    await noteModel.findOneAndUpdate({
+        _id: noteId
+    },{
+        title
+    })
+    res.json({
+        message: "Note Updated Successfully",
+    })
+})
+
+
+
+server.listen(3000,()=>{
+    console.log("Sever is running on port 3000");
+    
+})
